2930fcd17895cef995a609159c79e8e9c57b8cb2
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2016-07-26 10:16:41 -0400

    Initial Commit

---
+++ mkswap.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# size of swapfile in megabytes
+swapsize=4000
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [ $? -ne 0 ]; then
+  echo 'swapfile not found. Adding swapfile.'
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+  echo 'swapfile found. No changes made.'
+fi
+
+# output results to terminal
+df -h
+cat /proc/swaps
+cat /proc/meminfo | grep Swap
