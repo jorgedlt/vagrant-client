69b1fc59504148c08a2e463084926b07d0c7a52e
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2016-07-26 16:21:45 -0400

    Update v_bootstrap.sh

--- v_bootstrap.sh
+++ v_bootstrap.sh
@@ -14,11 +14,11 @@ apt-get install -y htop
 # apt-get install -y curl mysql-server-5.5
 # php5 php5-curl php5-mysql php5-cli php5-readline php5-xdebug php-pear hhvm
 # vim git redis-server redis-tools rabbitmq-server varnish varnish-doc nginx
-# nodejs npm xvfb firefox
 
-# Install nodejs
-# apt-get install -y nodejs
-# ln -s /usr/bin/nodejs /usr/local/bin/node
+# Install nodejs - deprecated method - Do Not Use
+#  apt-get install -y nodejs
+#  ln -s /usr/bin/nodejs /usr/local/bin/node
+#                      see - https://goo.gl/rPi7Cb
 
 # Install Apache
 # apt-get install -y apache 2> /dev/null2
@@ -28,7 +28,7 @@ apt-get install -y htop
 # fi
 
 # Install Java
-#  sudo apt-get -y install openjdk-7-jre              2> /dev/null
-#  sudo apt-get -y install openjdk-7-jdk lib32stdc++6 2> /dev/null
+  apt-get -y install openjdk-7-jre              2> /dev/null
+  apt-get -y install openjdk-7-jdk lib32stdc++6 2> /dev/null
 
 # -- fin
