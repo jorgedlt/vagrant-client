04cf3cebe66645c74d435d088eb17ddaf25e6e57
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2016-08-01 12:51:57 -0400

    added git to autoinstall

--- v_bootstrap.sh
+++ v_bootstrap.sh
@@ -9,7 +9,7 @@
 apt-get update
 apt-get -y upgrade
 #
-apt-get install -y htop
+apt-get install -y htop git
 
 # apt-get install -y curl mysql-server-5.5
 # php5 php5-curl php5-mysql php5-cli php5-readline php5-xdebug php-pear hhvm
@@ -27,8 +27,8 @@ apt-get install -y htop
 #  ln -fs /vagrant /var/www
 # fi
 
-# Install Java
-  apt-get -y install openjdk-7-jre              2> /dev/null
-  apt-get -y install openjdk-7-jdk lib32stdc++6 2> /dev/null
+# Install Java (open)
+  # apt-get -y install openjdk-7-jre              2> /dev/null
+  # apt-get -y install openjdk-7-jdk lib32stdc++6 2> /dev/null
 
 # -- fin
